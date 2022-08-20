66e43cd36de7bee36372568af47ef18c5f93a14e
Nosheen17 <hayat.mehreen17@gmail.com>
2022-08-20 14:05:17 +0500

    Declaring data members

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name, contact, age;
+    Button insert, update, delete, view;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
